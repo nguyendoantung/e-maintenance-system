84bf38a9fb31201608da8355810ee96ad8f8d852
tungnd44 <tungnd44@fpt.com.vn>
2023-04-13 23:05:30 +0700

    remove file .pyc and make it like 12 factors

--- back-end/app.py
+++ back-end/app.py
@@ -1,6 +1,13 @@
+from datetime import timedelta
+
 from flask import Flask
-from service.authen.blueprint import blueprint as authen_blueprint
+from flask_jwt_extended import JWTManager
+from service.api.authen.blueprint import blueprint as authen_blueprint
 
 app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "tungnd173451"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
+jwt = JWTManager(app)
 
 app.register_blueprint(authen_blueprint)
---
+++ back-end/service/api/authen/blueprint.py
@@ -0,0 +1,60 @@
+import json
+from datetime import datetime, timedelta, timezone
+
+from flask import Blueprint, Flask, jsonify, request
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    get_jwt,
+    get_jwt_identity,
+    jwt_required,
+    unset_jwt_cookies,
+)
+
+blueprint = Blueprint("authenticate", __name__, url_prefix="/authenticate")
+
+@blueprint.route('/token', methods=["POST"])
+def create_token():
+    email=request.json.get("email", "")
+    password = request.json.get("password", "")
+
+    if email != "tungn173451@sis.hust.edu.vn" or password != "WnnL4454":
+        return {"msg": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+    response = {"access_token":access_token}
+    return response
+
+@blueprint.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+
+@blueprint.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
+@blueprint.route("/profile", methods=["GET"])
+@jwt_required()
+def my_profile():
+    response_body = {
+        "name": "Nagato",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    }
+
+    return response_body
+
