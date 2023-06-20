fcd0991c12d847a05be222520c23ead035419338
CVHOANG-coder <mrgreencat99@gmail.com>
2023-06-20 17:37:06 +0700

    login page

--- front-end/e-maintenance-system/src/routers/contants.js
+++ front-end/e-maintenance-system/src/routers/contants.js
@@ -1,6 +1,7 @@
 export const LIST_ROUTE = {
     HOME_PAGE: "/",
     LOGIN_PAGE: "/login",
+    REGISTER: "/register",
     ADMIN_PAGE: "/:adminID/admin",
     BLOG: "/blog",
     CONTACT: "/contact",
