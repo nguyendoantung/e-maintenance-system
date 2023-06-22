45b935ea460fe0e18ea93844cb78ed8ed866a256
CVHOANG-coder <mrgreencat99@gmail.com>
2023-06-22 17:38:09 +0700

    multiple validation

--- front-end/e-maintenance-system/src/page/auth/Validators/RegisterSchema.js
+++ front-end/e-maintenance-system/src/page/auth/Validators/RegisterSchema.js
@@ -1,16 +1,22 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
-export const useRegisterValidator = () => {
+export const useRegisterValidator = (step) => {
     const errorMessages = "Không được bỏ trống trường này";
     const phoneRegExp = /(84|0[3|5|7|8|9])+([0-9]{8})\b/g;
     const registerSchema = yup
         .object({
             lastName: yup.string().required(errorMessages),
             firstName: yup.string().required(errorMessages),
-            userName: yup.string().required(errorMessages),
-            emailAddress: yup.string().required(errorMessages),
-            password: yup.string().required(errorMessages),
+            username: yup.string().required(errorMessages),
+            email: yup
+                .string()
+                .required(errorMessages)
+                .email("email không hợp lệ"),
+            password: yup
+                .string()
+                .required(errorMessages)
+                .min(8, "Phải có ít nhất 8 kí tự"),
             phone: yup
                 .string()
                 .required(errorMessages)
@@ -21,6 +27,5 @@ export const useRegisterValidator = () => {
                 .oneOf([true], "Cần đồng ý với điều khoản của chúng tôi"),
         })
         .required();
-
     return yupResolver(registerSchema);
 };
